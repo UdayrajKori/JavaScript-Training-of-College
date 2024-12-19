f30040f82d8621f78f8e4bce5f8cc1ef3ce9952d
UdayrajKori <udayrajkori456@gmail.com>
2024-12-19 12:40:42 +0545

    This is our JavaScript training

---
+++ Day 02/DataType.js
@@ -0,0 +1,37 @@
+// It is a type of data which a variable hold.
+// There are two types of data types in JavaScript: primitive and non-primitive or reference types.
+
+// 1.) String
+
+let mobile_num = "+977 9816474664";
+
+// 2.) Number
+
+let age = 25;
+
+// 3.) Undefined
+
+let x;
+console.log(x); // undefined
+
+// 4.) Null
+
+let y = null; // It is defined but has no value.
+
+// 5.) Boolean
+
+let isAdmin = true; // true or false
+
+console.log(mobile_num + " , " + age + " , " + x + " , " + isAdmin); // +977 9816474664 25 undefined null
+
+// TTruthy Vs Falsy Values
+
+// The follwoing are Falsy value
+
+// 1.) False
+// 2.) 0 (zero)
+// 3.) -0 (negative zero)
+// 4.) "" (empty string)
+// 5.) null
+// 6.) undefined
+// 7.) NaN (Not a Number)
---
+++ Day 02/Variable.js
@@ -0,0 +1,16 @@
+// Variable is a name given for the memory location.
+// It is a container that holds a certain types of values.
+// JavaScript is a loosely typed language, which means you don't have to specify the type of variable.
+
+// var name1 = "John Doe";
+
+var name1;
+name1 = "John Doe";
+
+let name2 = "Uday";
+
+const name3 = "Raj";
+
+console.log(name1);
+console.log(name2);
+console.log(name3);
