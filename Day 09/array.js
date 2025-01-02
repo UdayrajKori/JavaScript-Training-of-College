454f0bcfb3bd39133f08eec7ced26791ae523817
UdayrajKori <uday.kori24@radiantcollege.edu.np>
2025-01-02 14:09:42 +0545

    Today learn about array

---
+++ Day 09/array.js
@@ -0,0 +1,35 @@
+// const names = ["John", "Mary", "Jane", "Bob"];
+
+// console.log(names);
+// names.push("Uday"); // this is add element at the end of array
+
+// console.log(names);
+
+// names.pop(); // this is remove element at the end of array
+
+// console.log(names);
+
+// includes
+
+// indexOf, isArray, join, lastIndexOf, reduce, reverse,
+// shift, slice, splice, sort, findLastIndex, filter
+
+// shift element
+
+// let fruit = ["Apple", "Banana", "Cherry"];
+
+// console.log(fruit);
+
+// fruit.shift(); // this is remove element at the beginning of array
+
+// console.log(fruit);
+
+// unshift
+
+let fruits = ["Apple", "Banana", "Cherry"];
+
+console.log(fruits);
+
+fruits.unshift("Orange"); // this is add element at the beginning of array
+
+console.log(fruits);
---
+++ Day 09/assignment.js
@@ -0,0 +1,26 @@
+// let sum = 0;
+
+// for (let i = 1; i <= 50; i++) {
+//   sum = sum + i;
+// }
+
+// console.log(sum);
+
+// let num = [1, 2, 3, 4, 27, 6, 7, 8, 9, 10];
+// let sum1 = 0;
+// console.log(num.length);
+// for (let i = 0; i < num.length; i++) {
+//   sum1 = sum1 + num[i];
+//   console.log(num[i]);
+// }
+// console.log("______");
+// console.log(sum1);
+
+// array methods and properties
+
+const names = ["John", "Mary", "Jane", "Bob"];
+
+console.log(names);
+names.push("Uday");
+
+console.log(names);
