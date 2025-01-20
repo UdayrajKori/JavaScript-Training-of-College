2779ca805ebd04efcb2e28fe8213664abcd7566e
UdayrajKori <uday.kori24@radiantcollege.edu.np>
2025-01-20 14:19:06 +0545

    Array with function

---
+++ Day 11/assignment.js
@@ -0,0 +1,10 @@
+function myFunction(numbers) {
+  // parameter
+  console.log(numbers);
+
+  for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+  }
+}
+
+myFunction([1, 2, 3, 4, 5]); // arguments
---
+++ Day 11/function.js
@@ -0,0 +1,15 @@
+function myFun() {
+  console.log("This is function");
+  console.log("H! my dear");
+}
+
+myFun();
+
+// parameters and arguments
+
+function sum(a, b, c) {
+  // parameters
+  console.log(`The sum is: ${a + b + c}`);
+}
+
+sum(10, 10, 20); // argumnets
